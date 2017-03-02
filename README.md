b380fde084f57656416cc033e9a7a2fa12e1378d
Jonathan Mackenzie <jonmac1@gmail.com>
2017-03-02 13:44:25 +1030

    Fixed bugs, added readme

---
+++ README.md
@@ -0,0 +1,38 @@
+#  album2mp4
+Converts a folders of mp3 files into a mp4 single. Useful for uploading to youtube.
+It will also generate a handy .txt file describing when the start of each track is.
+
+## Requirements
+
+You will need:
+
+* Python
+* `pip install mutagen`, used for analysing mp3 tags and info
+* A recent version of ffmpeg or avconv 
+
+It should work on any system as long as you have those requirements, if you're 
+running windows, you will need to specify a font file path.
+## Usage
+
+Running `album2mp4.py --help` will yield:
+
+```
+usage: album2mp4.py [-h] [-f folder] [-o output] [-c cover] [-s]
+                    [-p font_path] [-v]
+
+Turn a folder of mp3 files into a single video suitable for youtube upload
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f folder, --folder folder
+                        The folder containing the mp3 files
+  -o output, --output output
+                        The name of the output file
+  -c cover, --cover cover
+                        Location of the cover art
+  -s, --song-title      Add the song title to the video
+  -p font_path, --font-path font_path
+                        Path to font file
+  -v, --verbose         Provide verbose output
+
+```
--- album2mp4.py
+++ album2mp4.py
@@ -5,14 +5,17 @@ import argparse
 import os
 import subprocess
 from PIL import Image
+import platform
 
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
+from scipy.weave.c_spec import file_converter
+
+is_windows = platform.system() == 'Windows'
 
 
 def which(program):
-    import platform
-    if platform.system() == 'Windows':
+    if is_windows:
         program += '.exe'
 
     def is_exe(fpath):
@@ -74,7 +77,7 @@ def do_process(folder, _cover, output, add_title, font, verbose):
         mp3 = MP3(song)
         print(out_name)
         temps.append(out_name)
-        descr.append("{0:0>2}:{1:0>2} ".format(int(time) / 60, int(time) % 60) + os.path.split(song.split('.mp3')[0])[-1])
+        descr.append("{0:0>2}:{1:0>2} - {2}".format(int(time) / 60, int(time) % 60, mp3info['title'][0]))
         args = [executable,
                 '-loop', '1',
                 '-y',
@@ -82,35 +85,37 @@ def do_process(folder, _cover, output, add_title, font, verbose):
                 '-i', song
                 ]
         if add_title:
-
             args += [
                 '-vf',
                 'drawtext=fontfile={}:text={}:fontcolor=white:fontsize=96:box=1:boxcolor=black@0.5:boxborderw=10:x=(w-tw)/2:y=(h/PHI)+th'.format(
-                    font, "{} - {}".format(mp3info['tracknumber'][0], mp3info['title'][0])
+                    font, "{}".format(mp3info['title'][0])
                 )]
         args += [
-                 '-c:v', 'libx264',
-                 '-c:a', 'copy',
-                 '-b:a', '320k',
-                 '-shortest',
-                 '-r', '5', out_name]
+            '-c:v', 'libx264',
+            '-c:a', 'copy',
+            '-b:a', '320k',
+            '-shortest',
+            '-r', '5', out_name]
         if verbose:
             print(" ".join(args))
         convert_out = subprocess.check_output(args, stderr=subprocess.STDOUT)
         if verbose:
             print(convert_out)
         time += mp3.info.length
-
-
+    filelisttxt = 'filelist.txt'
+    with open(filelisttxt, 'w') as file_list:
+        for line in temps:
+            file_list.write("file '{}'\n".format(line))
     all_files = 'concat:' + ('|'.join(temps))
     print("Fnames:", all_files)
-    out = subprocess.check_output([executable, '-y', '-i', all_files, '-c', 'copy', output], stderr=subprocess.STDOUT)
+    out = subprocess.check_output( [executable, '-y', '-f', 'concat', '-safe', '0', '-i', filelisttxt, '-c', 'copy', output])
     if verbose:
         print(out)
     with open(output + '.txt', 'w') as descr_file:
+        descr_file.write("{}\n".format(os.path.split(folder)[-1]))
         for line in descr:
             descr_file.write(line + os.linesep)
-        descr_file.write("Generated with album2mp4 https://github.com/jonnoftw/album2mp4")
+        descr_file.write("\nGenerated with album2mp4 https://github.com/jonnoftw/album2mp4")
 
 
 if __name__ == "__main__":
@@ -120,14 +125,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description='Turn a folder of mp3 files into a single video suitable for youtube upload')
-    parser.add_argument('-f', '--folder', metavar='folder', help='The folder containing the mp3 files', default=cwd)
+    parser.add_argument('-f', '--folder', metavar='folder', help='The folder containing the mp3 files', required=True,
+                        default=cwd)
     parser.add_argument('-o', '--output', metavar='output', help='The name of the output file',
                         default=os.path.split(os.getcwd())[-1] + '.mp4')
     parser.add_argument('-c', '--cover', metavar='cover', help='Location of the cover art', default=cwd + '/cover.jpg')
     parser.add_argument('-s', '--song-title', help='Add the song title to the video', action='store_true')
     parser.add_argument('-p', '--font-path', metavar='font_path', help='Path to font file',
                         default='/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf',
-                        required=False)
+                        required=is_windows)
     parser.add_argument('-v', '--verbose', help='Provide verbose output', action='store_true')
     args = parser.parse_args()
 
@@ -138,4 +144,11 @@ if __name__ == "__main__":
     font = args.font_path
     verbose = args.verbose
     print("Converting folder of mp3 to mp4")
+    from datetime import datetime
+    startTime = datetime.now()
     do_process(music_folder, cover, output, add_title, font, verbose)
+
+    print("Finished in {}",  datetime.now() - startTime)
+    print("Output in file:", output)
+
+
